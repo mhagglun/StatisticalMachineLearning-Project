ca0e1180478180accdf38459b1f38adeac129d90
Fred Lindahl <47213636+lindahlf@users.noreply.github.com>
2020-10-02 13:54:45 +0200

    underwhelming network

--- neuralntwrk.py
+++ neuralntwrk.py
@@ -2,11 +2,16 @@ import numpy as np
 import tensorflow as tf
 import seaborn as sns
 import pandas as pd
+import matplotlib.pyplot as plt
 from tensorflow import keras
 from sklearn.metrics import accuracy_score
+from sklearn.preprocessing import StandardScaler
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Dense
 from tensorflow.keras.layers import Dropout
+from tensorflow.keras import initializers
+from tensorflow.keras import regularizers
+from tensorflow.keras.regularizers import l2
 
 sns.set_style("dark")
 
@@ -29,21 +34,31 @@ test_labels = test_data['Label']
 train_data = train_data.iloc[:,0:11]
 test_data = test_data.iloc[:,0:11]
 
+train_data = (train_data - train_data.mean())/train_data.std()
+test_data = (test_data - test_data.mean())/test_data.std()
+
 train_data = train_data.to_numpy()
 test_data = test_data.to_numpy()
 
 train_labels = train_labels.to_numpy()
 test_labels = test_labels.to_numpy()
 
+# Scale dataset
+
 ###################################################
 
 # define and fit the final model
-model = Sequential()
-model.add(Dense(64, input_dim=11, activation='relu'))
-model.add(Dense(128, activation='relu'))
-model.add(Dense(1, activation='sigmoid'))
-model.compile(loss='binary_crossentropy', optimizer='adam')
-model.fit(train_data, train_labels, epochs=200, verbose=0)
 
+initializer = tf.keras.initializers.RandomNormal(mean=0.0, stddev=0.05, seed=None)
+
+model = Sequential()
+model.add(Dense(64, input_dim=11, activation='relu', kernel_initializer=initializer))
+model.add(Dense(128, activation='relu', kernel_initializer=initializer))
+model.add(Dense(8, activation='relu', kernel_initializer=initializer))
+model.add(Dense(1, activation='sigmoid'))
+model.compile(loss='binary_crossentropy', optimizer='adam', metrics='accuracy')
+history = model.fit(train_data, train_labels, epochs=50, batch_size = 1, validation_split = 0.1)
+plt.plot(history.history['val_loss'])
+plt.show()
 predictions = model.predict_classes(test_data)
 print(accuracy_score(test_labels,predictions))
